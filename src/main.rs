d6c726c33c8fd3d4031cc9337470036c15c1aabf
Albert Viaplana <pichi.pride@gmail.com>
2020-05-02 16:21:35 +0200

    Persistance interface.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 mod stocks;
+mod persistance;
 
 use stocks::*;
+use persistance::*;
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let persistance = StockRepository::new();
+
+    println!("Stored: ");
+    for entry in persistance.get_entries() {
+        println!("{:?}", entry);
+    }
+
     let stocks_repo = StockRepo::new();
     stocks_repo.get_stock_price("T").await.unwrap();
     Ok(())
